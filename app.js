205583c858c00b5b81621c655533507232f1f309
RQcoder <RQcoder7-86@users.noreply.github.com>
2025-08-03 11:46:37 +0500

    Initial commit of Qasim Portfolio project

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-=======
+
 const express = require("express");
 const app = express();
 const path = require("path");
